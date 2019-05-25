4228f0508be47a1b41ebd16f957af2f941f9d336
AvianAnalyst <aviananalyst@gmail.com>
2019-05-25 16:07:12 -0400

    Updated functional test to test for multiple possible lists/users with unique URLs Added functionality to dynamically wait for page loads instead of hard coded wait times.

--- lists/views.py
+++ lists/views.py
@@ -3,7 +3,10 @@ from lists.models import Item
 
 
 # Create your views here.
-# TODO: support more than one list?
+# TODO: Adjust model so that items are associated with different lists
+# TODO: Support unique URLs for each list
+# TODO: Add a URL for creating a new list via POST
+# TODO: Add URLS for adding a new item to an existing list via POST
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
