2cae8bc737c505b4399892bfb62f03c3ff1a239f
Kylan <aviananalyst@gmail.com>
2019-09-18 12:41:35 -0700

    Commiting to test fabric2 rewrite, should be near final

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,49 +1,60 @@
 import random
-from fabric.contrib.files import  append, exists
-from fabric.api import cd, env, local, run
+
+from fabric import task
+from patchwork.files import append, exists
 
 
 REPO_URL = 'https://github.com/aviananalyst/testdrivendev.git'
 
 
-def deploy():
-    site_folder = f'/home/{env.user}/sites/{env.host}'
-    run(f'mkdir -p {site_folder}')
-    with cd(site_folder):
-        _get_latest_source()
-        _update_virtualenv()
-        _create_or_update_dotenv()
-        _update_static_files()
-        _update_database()
-
-
-def _get_latest_source():
-    if exists('.git'):
-        run('git fetch')
+def _get_latest_source(c, current_commit):
+    print('check if .git exists')
+    if exists(c, '.git'):
+        print('it does, git fetch')
+        c.run('git fetch')
     else:
-        run(f'git clone {REPO_URL} .')
-    current_commit = local("git log -n 1 --format=%h", capture=True)
-    run(f'git reset --hard {current_commit}')
+        print('it does not, git clone repo')
+        c.run(f'git clone {REPO_URL} .')
+
+    print('update to current commit')
+    c.run(f'git reset --hard {current_commit}')
 
 
-def _update_virtualenv():
-    run('pipenv sync')
-
-
-def _create_or_update_dotenv():
+def _create_or_update_dotenv(c):
+    print('add django debug env var')
     append('.env', 'DJANGO_DEBUG_FALSE=y')
-    append('.env', f'SITENAME={env.host}')
-    current_contents = run('cat .env')
+    print('add sitename env var')
+    append('.env', f'SITENAME={c.host}')
+    print('check if secret key exists')
+    current_contents = c.run('cat .env')
     if 'DJANGO_SECRET_KEY' not in current_contents:
+        print('it does not, make a new one and add it')
         new_secret = ''.join(random.SystemRandom().choices(
             'abcdefghijklmnopqrstuvwxyz0123456789', k=50
         ))
         append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+    else:
+        print('it does')
 
 
-def _update_static_files():
-    run('pipenv run collect_static')
+@task
+def deploy(c):
+    site_folder = f'/home/{c.user}/sites/{c.host}'
+    print(f'site_folder set to: {site_folder}')
+    print('ensure site directory exists')
+    c.run(f'mkdir -p {site_folder}')
+    print('get current commit')
+    current_commit = c.local("git log -n 1 --format=%h")
+    print('following commands done in site_folder on remote machine')
+    with c.cd(site_folder):
+        print('update source')
+        _get_latest_source(c, current_commit)
+        print('update dependencies')
+        c.run('pipenv sync')
+        print('update dotenv')
+        _create_or_update_dotenv(c)
+        print('collect static files')
+        c.run('pipenv run collect_static')
+        print('perform migrations')
+        c.run('pipenv run update_db')
 
-
-def _update_database():
-    run('pipenv run update_db')
